d6fb18ad3f7a84445d6676b36e653418464bb9ff
adclleva <arvin.lleva@yahoo.com>
2020-08-09 21:00:14 -0700

    Added the fetchOrders action to align with the orders reducer to fetch the order data from firebase, in order to use the data within the orders screen. Able to have the loading indicator as well

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,13 +1,41 @@
-import React from "react";
-import { FlatList, Text, Platform } from "react-native";
-import { useSelector } from "react-redux"; // useSelector taps into the redux store
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  Platform,
+  ActivityIndicator,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux"; // useSelector taps into the redux store
 import { Item, HeaderButtons } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
 import OrderItem from "../../components/shop/OrderItem";
+import * as ordersActions from "../../store/actions/orders";
+import Colors from "../../constants/Colors";
 
 const OrdersScreen = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(ordersActions.fetchOrders()).then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}
@@ -44,3 +72,11 @@ OrdersScreen.navigationOptions = (navData) => {
 };
 
 export default OrdersScreen;
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -1,4 +1,4 @@
-import { ADD_ORDER } from "../actions/orders";
+import { ADD_ORDER, SET_ORDERS } from "../actions/orders";
 import Order from "../../models/order";
 
 const initialState = {
@@ -7,6 +7,11 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    // all the logic/work is don within the action creator for SET_ORDERS
+    case SET_ORDERS:
+      return {
+        orders: action.orders,
+      };
     case ADD_ORDER:
       // we'll be using the date as the psuedo dummy unique id, we'll be using the server to create new ones
       const newOrder = new Order(
